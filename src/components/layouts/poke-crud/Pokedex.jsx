4bad9088ac7368bbc4a6e19a675f91e911f06b2b
Valentina <valentinabetancur0607@gmail.com>
2023-07-13 12:16:05 -0500

    avance de pokedex

--- src/components/layouts/poke-crud/Pokedex.jsx
+++ src/components/layouts/poke-crud/Pokedex.jsx
@@ -1,12 +1,17 @@
 import Header from "../../helpers/Header";
 import { useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
+import { dataBase } from "./config/pokeConection";
 
 const Pokedex = () => {
   const [pokeLista, setPokeLista] = useState();
-  const mostrarPokeLista = () => {
-    
-  }
+  const mostrarPokeLista = async () => {
+    const pokeCollection = collection(dataBase, "pokemon");
+    const data = await getDocs(pokeCollection);
+    setPokeLista(data.docs.map((doc) => ({ ...doc.data() })));
+    console.log(data.docs.map((doc) => ({ ...doc.data() })));
+  };
+  //   mostrarPokeLista()
   return (
     <section>
       <Header />
